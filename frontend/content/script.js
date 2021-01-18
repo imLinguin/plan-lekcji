1db16b4bf50e4a38dae431c7b2b0027eecdff596
Olek Baran <olek@olek-baran.pl>
2021-01-18 16:22:57 +0100

    data i zegar

--- frontend/content/script.js
+++ frontend/content/script.js
@@ -4,3 +4,7 @@ let y = n.getFullYear();
 let m = n.getMonth() + 1 >= 10 ? n.getMonth() + 1 : "0" + (n.getMonth() + 1);
 let d = n.getDate();
 document.querySelector(".date").innerHTML = d + "." + m + "." + y;
+
+const days = ['Niedziela','Poniedziałek','Wtorek','Środa','Czwartek','Piątek','Sobota'];
+
+document.querySelector(".day-of-week").innerHTML = days[n.getDay()];
