b9d877274baa5a1f45ee0d9ef113078802e35fc5
Olek Baran <olek@olek-baran.pl>
2021-01-21 13:54:35 +0100

    footer-popup

--- frontend/content/popup/popup.js
+++ frontend/content/popup/popup.js
@@ -2,11 +2,18 @@ const { ipcRenderer } = require("electron");
 const saveButton = document.querySelector(".save");
 const restoreButton = document.querySelector(".restore");
 const religiaCheck = document.querySelector(".religia");
+const klasaBox = document.querySelector(".klasa");
+const grupyBox = document.querySelector(".grupa");
+const themeSelect = document.querySelector(".motyw")
 let cache = ipcRenderer.sendSync("get-preferences", null);
 if (cache) SetPreferences(cache);
 saveButton.addEventListener("click", () => {
+  if(klasaBox.value && klasaBox.value)
   ipcRenderer.send("closensave-popup", {
     religia: religiaCheck.checked,
+    klasa: klasaBox.value,
+    grupa: grupyBox.value,
+    motyw: themeSelect.value,
   });
 });
 
@@ -14,6 +21,8 @@ restoreButton.addEventListener("click", () => {
   SetPreferences(cache);
 });
 function SetPreferences(data) {
-  console.log(data.religia);
   religiaCheck.checked = data.religia;
+  klasaBox.value = data.klasa;
+  grupyBox.value = data.grupa;
+  themeSelect.value = data.motyw;
 }
